3de6c6b69f58b2077e03cac81ddbd4a6c6b288ca
vladbv <vladimirbhelp@gmail.com>
2021-10-13 16:51:31 +0300

    Changes

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Person from './Person.js'; //Component
+import Header from './components/Header.js';
+import Button from './components/Button.js';
+
+function App() {
+
+	return (
+<div className="container">
+<Header title={'Task Tracker'} />
+		</div>
+	)
+
+
+}
+
+export default App;
